ec5669dddcaa5638b21253c8846370304974a913
abdulrahimsulthan <rahimcsestudies@gmail.com>
2025-03-15 03:19:04 +0530

    ✨ feat(permission): push notification
    Get user runtime permission for Notification
    + It shows the popup for android 33+ devices.
    + Get the FCM token and log it for testing.
    + FCM token is need to be saved in backend that can be used to send notification to the target device.

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,26 @@
-import { Text, View } from "react-native";
+import { PermissionsAndroid, Text, View } from "react-native";
+import messaging from '@react-native-firebase/messaging'
+import { useEffect } from "react";
+
+const requestUserPermission = async () => {
+  PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS)
+  const authStatus = await messaging().requestPermission();
+  const enabled =
+    authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+    authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+  if (enabled) {
+    console.log('Authorization status:', authStatus);
+    const token = await messaging().getToken()
+    console.log('FCM token ', token)
+  }
+};
 
 export default function Index() {
+  useEffect(() => {
+    requestUserPermission()
+  }, [])
+
   return (
     <View className="flex-1 justify-center items-center">
       <Text className="text-5xl text-dark-200 font-bold">Welcome!</Text>
