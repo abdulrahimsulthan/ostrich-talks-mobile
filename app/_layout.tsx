527fa6a38b987be114b1b359dec784101d237214
abdulrahimsulthan <rahimcsestudies@gmail.com>
2025-04-28 00:56:13 +0530

    ✨ feat(layout): safe area
    Created safe area for content to show in mobile devices

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,24 @@
 import { Stack } from "expo-router";
 import './global.css'
+import { SafeAreaProvider, useSafeAreaInsets } from "react-native-safe-area-context";
+import { StatusBar } from "expo-status-bar";
+import { View } from "react-native";
 
 export default function RootLayout() {
-  return <Stack />;
+  const insets = useSafeAreaInsets();
+
+  return <SafeAreaProvider>
+    <View className="flex-1 bg-background" style={{
+      paddingTop: insets.top,
+      paddingBottom: insets.bottom,
+      paddingLeft: insets.left,
+      paddingRight: insets.right
+      }}>
+      <Stack screenOptions={{ headerShown: false }} >
+        <Stack.Screen name="index" />
+        <Stack.Screen name="(auth)/index" />
+      </Stack>
+    </View>
+    <StatusBar style="dark" />
+  </SafeAreaProvider>
 }
