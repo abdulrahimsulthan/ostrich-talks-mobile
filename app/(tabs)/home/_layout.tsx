a876b4e8a985155f73f5c54bfc88fb4c0a558a95
abdulrahimsulthan <rahimcsestudies@gmail.com>
2025-04-29 05:33:36 +0530

    ✨ feat(layout): add new screens for streak, shop, and life with formSheet presentation

--- app/(tabs)/home/_layout.tsx
+++ app/(tabs)/home/_layout.tsx
@@ -3,6 +3,16 @@ import { Stack, Tabs } from "expo-router";
 export default function HomeLayout() {
     return (
         <Stack screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="index" />
+            <Stack.Screen name="streak" options={{
+                presentation: "formSheet",
+            }}/>
+            <Stack.Screen name="shop" options={{
+                presentation: "formSheet",
+            }}/>
+            <Stack.Screen name="life" options={{
+                presentation: "formSheet",
+            }}/>
         </Stack>
     )
 };
