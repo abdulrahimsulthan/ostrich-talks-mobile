6c13455db0d221996c1cc5c2f17bca50389322d7
abdulrahimsulthan <rahimcsestudies@gmail.com>
2025-04-29 22:29:00 +0530

    ✨ feat(style): format
    Code formatted.

--- app/(tabs)/home/streak.tsx
+++ app/(tabs)/home/streak.tsx
@@ -5,66 +5,66 @@ import colors from '@/constants/colors'
 
 const streak = () => {
   return (
-  <ScrollView>
-    <View className='bg-background items-center justify-center'>
+    <ScrollView>
+      <View className='bg-background items-center justify-center'>
 
-      {/* streak banner */}
-      <View className='flex-row items-center justify-between bg-primary p-4 rounded-lg shadow-md my-4 w-[90%]'>
-        <View>
-          <Text className='text-3xl text-white py-4'>1 Day Streak</Text>
-          <Text className='text-white mb-1'> This is your longest streak, don't stop.</Text>
+        {/* streak banner */}
+        <View className='flex-row items-center justify-between bg-primary p-4 rounded-lg shadow-md my-4 w-[90%]'>
+          <View>
+            <Text className='text-3xl text-white py-4'>1 Day Streak</Text>
+            <Text className='text-white mb-1'> This is your longest streak, don't stop.</Text>
+          </View>
+          <MaterialIcons name="whatshot" size={40} color="white" />
         </View>
-        <MaterialIcons name="whatshot" size={40} color="white" />
-      </View>
 
-      {/* streak timer CTA for lessions*/}
-      <View className='flex-row items-center justify-start p-4 rounded-lg shadow-md my-4 w-[90%]'>
-        <Ionicons name="timer-outline" size={24} color="red" />
-        <View className='ml-2 p-2'>
-          <Text className='text-textPrimary'>3 hours until your streak resets!</Text>
-          <Text className='text-textPrimary mt-2'>START LESSION</Text>
+        {/* streak timer CTA for lessions*/}
+        <View className='flex-row items-center justify-start p-4 rounded-lg shadow-md my-4 w-[90%]'>
+          <Ionicons name="timer-outline" size={24} color="red" />
+          <View className='ml-2 p-2'>
+            <Text className='text-textPrimary'>3 hours until your streak resets!</Text>
+            <Text className='text-textPrimary mt-2'>START LESSION</Text>
+          </View>
         </View>
-      </View>
 
-      {/* streak history */}
-      <View className='flex-1 items-center justify-between bg-primary p-4 rounded-lg shadow-md my-4 w-[90%]'>
-        <View className='w-full flex-row items-center justify-between'>
+        {/* streak history */}
+        <View className='flex-1 items-center justify-between bg-primary p-4 rounded-lg shadow-md my-4 w-[90%]'>
+          <View className='w-full flex-row items-center justify-between'>
             <Text className='text-lg text-white'>Streak Calendar</Text>
-           <MaterialIcons name="history" size={24} color="white" />
-        </View>
-        <View className='w-full flex-1 items-center justify-end bg-background mt-4 h-[275] p-3 shadow-md rounded-lg'>
-          <View className='flex-row justify-end items-center w-full p-2'>
-            <Text className='text-lg text-primary'>Last 7 days</Text>
-            <MaterialIcons name="arrow-forward-ios" size={24} color={colors.primary} />
+            <MaterialIcons name="history" size={24} color="white" />
+          </View>
+          <View className='w-full flex-1 items-center justify-end bg-background mt-4 h-[275] p-3 shadow-md rounded-lg'>
+            <View className='flex-row justify-end items-center w-full p-2'>
+              <Text className='text-lg text-primary'>Last 7 days</Text>
+              <MaterialIcons name="arrow-forward-ios" size={24} color={colors.primary} />
+            </View>
           </View>
         </View>
-      </View>
-     
-      {/* Streak Goal */}
-      <View className='flex-1 items-center justify-between p-4 rounded-lg shadow-md my-4 w-[90%]'>
-        <View className='w-full flex-row items-center justify-between p-2'>
+
+        {/* Streak Goal */}
+        <View className='flex-1 items-center justify-between p-4 rounded-lg shadow-md my-4 w-[90%]'>
+          <View className='w-full flex-row items-center justify-between p-2'>
             <Text className='text-xl text-primary'>Streak Goal</Text>
-           
-        </View>
-        <View className='flex-row mb-2 justify-between items-center w-full p-2'>
-          <Text className='text-xl text-wrap w-4/5'>You're 3 days into your 30 days goal</Text>
-          <MaterialIcons name="surfing" size={40} color={colors.primary} />
-          
-        </View>
-        <View className='flex-row mb-2'>
-          <View className='flex-1 mx-4 h-3 bg-gray-300 rounded-full'>
-              <View className='h-3 bg-primary rounded-full' style={{ width: '10%' }} />
+
           </View>
-        </View>
-        
+          <View className='flex-row mb-2 justify-between items-center w-full p-2'>
+            <Text className='text-xl text-wrap w-4/5'>You're 3 days into your 30 days goal</Text>
+            <MaterialIcons name="surfing" size={40} color={colors.primary} />
+
+          </View>
+          <View className='flex-row mb-2'>
+            <View className='flex-1 mx-4 h-3 bg-gray-300 rounded-full'>
+              <View className='h-3 bg-primary rounded-full' style={{ width: '10%' }} />
+            </View>
+          </View>
+
           <View className='flex-row justify-end items-center w-full p-2'>
             <Text className='text-lg text-primary'>Set a goal</Text>
             <MaterialIcons name="arrow-forward-ios" size={24} color={colors.primary} />
           </View>
-      </View>
+        </View>
 
-    </View>
-  </ScrollView>
+      </View>
+    </ScrollView>
   )
 }
 
