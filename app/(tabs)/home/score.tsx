64a6ec69303fffd72b15959189250dc499eaa637
abdulrahimsulthan <rahimcsestudies@gmail.com>
2025-04-30 15:56:11 +0530

    ✨ feat(score): Screen update
    Update layout and improve score display

--- app/(tabs)/home/score.tsx
+++ app/(tabs)/home/score.tsx
@@ -3,16 +3,19 @@ import React from 'react'
 
 const score = () => {
     return (
-        <View className='items-center justify-center'>
-            <View className='w-[90%] flex-row items-center justify-between px-4'>
-                <Text className='text-lg font-bold'>26</Text>
-                <View className='flex-1 mx-4 h-4 bg-gray-300 rounded-full'>
-                    <View className='h-4 bg-primary rounded-full' style={{ width: '95%' }} />
+        <View className='flex-1 bg-background items-center '>
+            <View className='items-center justify-center my-4 py-4'>
+                <View className='w-[90%] flex-row items-center justify-between px-4'>
+                    <Text className='text-lg font-bold'>26</Text>
+                    <View className='flex-1 mx-4 h-4 bg-gray-300 rounded-full'>
+                        <View className='h-4 bg-primary rounded-full' style={{ width: '70%' }} />
+                    </View>
+                    <Text className='text-lg font-bold'>27</Text>
                 </View>
-                <Text className='text-lg font-bold'>27</Text>
+                <Text className='text font-bold'>Your English Score is 26</Text>
+                <Text className='text-xl font-bold'>More about Score</Text>
             </View>
-            <Text className='text font-bold'>Your English Score is 26</Text>
-            <Text className='text-xl font-bold'>More about Score</Text>
+
         </View>
     )
 }
