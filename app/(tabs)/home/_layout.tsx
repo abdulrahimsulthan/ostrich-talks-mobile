30f73687be94a8b555b1cd01e3b8fa0a3a6b1aec
abdulrahimsulthan <rahimcsestudies@gmail.com>
2025-04-30 18:49:28 +0530

    ✨ feat(shop): Screen
    Add shop layout with subscription CTA and special offers

--- app/(tabs)/home/_layout.tsx
+++ app/(tabs)/home/_layout.tsx
@@ -1,30 +1,40 @@
+import colors from "@/constants/colors";
+import { MaterialIcons } from "@expo/vector-icons";
 import { Stack } from "expo-router";
+import { Text, View } from "react-native";
 
 export default function HomeLayout() {
+    const shopRight = () =>
+        <View className="flex-row items-center justify-center gap-2">
+            <MaterialIcons name="diamond" size={20} color={colors.primary} />
+            <Text className="text-textSecondary">100</Text>
+        </View>
+
+
     return (
         <Stack screenOptions={{ headerShown: true }}>
-            <Stack.Screen name="index" options={{ headerShown: false} }/>
-
+            <Stack.Screen name="index" options={{ headerShown: false }} />
             <Stack.Screen name="streak" options={{
                 presentation: "fullScreenModal",
                 title: "Streak",
-            }}/>
+            }} />
             <Stack.Screen name="shop" options={{
                 presentation: "fullScreenModal",
                 title: "Shop",
-            }}/>
+                headerRight: shopRight,
+            }} />
             <Stack.Screen name="heart" options={{
                 presentation: "fullScreenModal",
                 title: "Hearts",
-            }}/>
+            }} />
             <Stack.Screen name="plan" options={{
                 presentation: "fullScreenModal",
                 title: "Plan",
-            }}/>
+            }} />
             <Stack.Screen name="score" options={{
                 presentation: "fullScreenModal",
                 title: "Score",
-            }}/>
+            }} />
         </Stack>
     )
 };
--- app/(tabs)/home/shop.tsx
+++ app/(tabs)/home/shop.tsx
@@ -1,11 +1,64 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView, TouchableOpacity } from 'react-native'
 import React from 'react'
+import { MaterialIcons } from '@expo/vector-icons'
+import colors from '@/constants/colors'
 
 const shop = () => {
   return (
-    <View>
-      <Text>shop</Text>
-    </View>
+    <ScrollView className='flex-1 bg-background'>
+      <View className='bg-background items-center '>
+
+        {/* CTA subscription */}
+        <View className='w-[90%] jsutify-center items-center bg-background p-4 rounded-lg shadow-md mt-4'>
+          <Text className='text-textPrimary text-2xl font-bold'>Pro</Text>
+          <Text className='text-textSecondary text-base'>Unlock all features with a subscription</Text>
+          <TouchableOpacity className='bg-primary rounded-lg p-4 mt-4 w-full'>
+            <Text className='text-white text-center text-lg'>Try 1 week for Free</Text>
+          </TouchableOpacity>
+        </View>
+
+        {/* Special offers */}
+        <View className='flex-1 my-4 bg-background justify-center'>
+          <View className='mb-2 w-[90%]'>
+            <Text className='text-2xl font-bold'>Special Offers</Text>
+          </View>
+
+          <View className='flex-row items-center justify-between p-6 rounded-lg shadow-md mb-2 w-[90%] gap-4'>
+            <MaterialIcons name="lock" size={60} color={colors.textSecondary} />
+            <View className='w-[75%]'>
+              <Text className='font-bold text-xl'>Early Bird Chest</Text>
+              <Text className='text-wrap'>Do a lession between 6:00 am and noon to unlock this chest.</Text>
+              <Text className='text-lg'>Locked</Text>
+            </View>
+          </View>
+
+          <View className='flex-row items-center justify-between p-6 rounded-lg shadow-md mb-2 w-[90%] gap-4'>
+            <MaterialIcons name="lock" size={60} color={colors.textSecondary} />
+            <View className='w-[75%]'>
+              <Text className='font-bold text-xl'>Night Owl Chest</Text>
+              <Text className='text-wrap'>Do a lession between 6:00 pm and midnight to unlock this chest.</Text>
+              <Text className='text-lg'>Locked</Text>
+            </View>
+          </View>
+
+          <View className='flex-row items-center justify-between p-6 rounded-lg shadow-md mb-2 w-[90%] gap-4'>
+            <MaterialIcons name="lock-open" size={60} color={colors.textSecondary} />
+            <View className='w-[75%]'>
+              <Text className='font-bold text-xl'>Free Chest</Text>
+              <Text className='text-wrap'>Watch an ad to earn up to 15 gems.</Text>
+              <TouchableOpacity className='bg-primary rounded-md px-4 py-2 mt-2 '>
+                <Text className='text-lg text-center text-white font-bold'>CLAIM CHEST</Text>\
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+
+
+      </View>
+
+
+
+    </ScrollView>
   )
 }
 
