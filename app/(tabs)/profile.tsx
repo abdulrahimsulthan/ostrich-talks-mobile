64951b0f5e22e7fd02d0c2f55e6041d7fbf2dfc3
abdulrahimsulthan <rahimcsestudies@gmail.com>
2025-05-01 23:10:10 +0530

    ✨ feat(profile): header section
    Enhance profile screen with avatar and scrollable layout

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,11 +1,20 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView } from 'react-native'
 import React from 'react'
+import Profile from "@/assets/avatars/profile.svg"
 
 const profile = () => {
   return (
-    <View>
-      <Text>profile</Text>
-    </View>
+    <ScrollView className='flex-1 bg-background'>
+      {/* Header */}
+      <View className='flex-1 items-center justify-center mt-4 mb-2 p-4 bg-background'>
+        <View className='h-40  w-40 rounded-full shadow-lg items-center justify-center'>
+          <Profile width={100} height={100} />
+        </View>
+        <Text className="my-2">John Doe</Text>
+        <Text>Joined on 28 Apr 2025</Text>
+      </View>
+
+    </ScrollView>
   )
 }
 
