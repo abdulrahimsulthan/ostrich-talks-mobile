31c6e2bf80279dcedb02c8d399305ff0edf4eaef
abdulrahimsulthan <rahimcsestudies@gmail.com>
2025-05-01 22:17:28 +0530

    ✨ feat(configure): support svg
    Configured metro file and typescript declaration for
    + Support svg as React Native component

--- metro.config.js
+++ metro.config.js
@@ -3,4 +3,18 @@ const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname)
 
+// Update transformer for SVGs
+config.transformer = {
+    ...config.transformer,
+    babelTransformerPath: require.resolve("react-native-svg-transformer/expo"),
+  };
+  
+// Update resolver for .svg support
+config.resolver = {
+    ...config.resolver,
+    assetExts: config.resolver.assetExts.filter(ext => ext !== "svg"),
+    sourceExts: [...config.resolver.sourceExts, "svg"],
+};
+  
+
 module.exports = withNativeWind(config, { input: './app/global.css' })
