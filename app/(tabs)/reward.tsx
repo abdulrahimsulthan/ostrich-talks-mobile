f198881622aa3d86c574c413e80d651b72c72fb9
abdulrahimsulthan <rahimcsestudies@gmail.com>
2025-05-08 05:21:55 +0530

    ✨ feat(reward): challenge section
    Enhance Weekly Challenge UI with progress bar and time display

--- app/(tabs)/reward.tsx
+++ app/(tabs)/reward.tsx
@@ -1,17 +1,35 @@
 import { View, Text, ScrollView, TouchableOpacity } from 'react-native'
 import React from 'react'
+import { MaterialCommunityIcons, MaterialIcons } from '@expo/vector-icons'
+import colors from '@/constants/colors'
 
 const reward = () => {
   return (
     <ScrollView className='bg-background'>
-      <View className='w-[90%] mx-auto'>
+      <View className='w-[90%] mx-auto mt-2'>
 
         {/* Weekly Challenge */}
-        <View className='my-2 w-full'>
-          <Text className='font-bold text-xl'>Weekly Challenge</Text>
-          <Text className='text-sm text-gray-500'>4 DAYS</Text>
-          <Text>Earn 12 quest points</Text>
-          <Text>3 / 12</Text>
+        <View className='flex-row justify-between items-center my-2 w-full shadow-md rounded-lg p-4 bg-background'>
+          <View className='w-[60%]'>
+            <View className='mb-2'>
+              <Text className='font-bold text-xl mb-1'>Weekly Challenge</Text>
+              <View className='flex-row items-center'>
+                <MaterialIcons className='mr-2' name="timelapse" size={20} color={colors.primary} />
+                <Text className='text-sm text-gray-500'>4 DAYS</Text>
+              </View>
+            </View>
+
+            <Text className='text-lg mb-2'>Earn 12 quest points</Text>
+
+            {/* progress bar */}
+            <View className='flex-1 h-6 bg-gray-300 rounded-full w-full justify-center items-center'>
+              <Text className='text-textPrimary z-10'>3 / 12</Text>
+              <View className='absolute left-0 h-6 bg-primary rounded-full ' style={{ width: '25%' }} >
+              </View>
+            </View>
+          </View>
+
+          <MaterialCommunityIcons className='mr-2' name="treasure-chest" size={100} color={colors.primary} />
         </View>
 
         {/* Friends Quest */}
