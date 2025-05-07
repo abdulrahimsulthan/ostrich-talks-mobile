f51902bcfa381a7d569c78cf2138179505e05049
abdulrahimsulthan <rahimcsestudies@gmail.com>
2025-05-08 04:49:01 +0530

    ✨ feat(practice): Template Screen
    Implement conversation and collections sections with scrollable view

--- app/(tabs)/practice.tsx
+++ app/(tabs)/practice.tsx
@@ -1,11 +1,39 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView, TouchableOpacity } from 'react-native'
 import React from 'react'
+import { MaterialIcons } from '@expo/vector-icons'
 
 const practice = () => {
   return (
-    <View>
-      <Text>practice</Text>
-    </View>
+    <ScrollView className='bg-background'>
+      <View className='w-[90%] mx-auto'>
+        {/* Conversation */}
+        <View className='my-4 gap-4'>
+          <Text className='text-xl '>Conversation</Text>
+          <TouchableOpacity className='flex-row items-center justify-between bg-primary p-4 rounded-lg shadow-md'>
+            <Text className='text-white'>Speak</Text>
+            <MaterialIcons name='mic' color="white" size={24}/>
+          </TouchableOpacity>
+          <TouchableOpacity className='flex-row items-center justify-between bg-primary p-4 rounded-lg shadow-md'>
+            <Text className='text-white'>Listen</Text>
+            <MaterialIcons name='headphones' color="white" size={24}/>
+          </TouchableOpacity>
+        </View>
+
+        {/* Your Collections */}
+        <View className='my-4 gap-4'>
+          <Text className='text-xl '>Your Collections</Text>
+          <TouchableOpacity className='flex-row items-center justify-between bg-primary p-4 rounded-lg shadow-md'>
+            <Text className='text-white'>Mistakes</Text>
+            <MaterialIcons name='repeat' color="white" size={24}/>
+          </TouchableOpacity>
+          <TouchableOpacity className='flex-row items-center justify-between bg-primary p-4 rounded-lg shadow-md'>
+            <Text className='text-white'>Stories</Text>
+            <MaterialIcons name='book' color="white" size={24}/>
+          </TouchableOpacity>
+        </View>
+        
+      </View>
+    </ScrollView >
   )
 }
 
