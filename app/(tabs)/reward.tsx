35d8d1f9fc36b7300b9cdf3e917de06e65635a39
abdulrahimsulthan <rahimcsestudies@gmail.com>
2025-05-08 06:50:05 +0530

    ✨ feat(reward): friends quest section
    Enhance Friends Quest section with progress bar and status

--- app/(tabs)/reward.tsx
+++ app/(tabs)/reward.tsx
@@ -2,6 +2,7 @@ import { View, Text, ScrollView, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { MaterialCommunityIcons, MaterialIcons } from '@expo/vector-icons'
 import colors from '@/constants/colors'
+import { Image } from 'expo-image'
 
 const reward = () => {
   return (
@@ -9,7 +10,7 @@ const reward = () => {
       <View className='w-[90%] mx-auto mt-2'>
 
         {/* Weekly Challenge */}
-        <View className='flex-row justify-between items-center my-2 w-full shadow-md rounded-lg p-4 bg-background'>
+        <View className='flex-row justify-between items-center my-4 w-full shadow-md rounded-lg p-4 bg-background'>
           <View className='w-[60%]'>
             <View className='mb-2'>
               <Text className='font-bold text-xl mb-1'>Weekly Challenge</Text>
@@ -33,23 +34,52 @@ const reward = () => {
         </View>
 
         {/* Friends Quest */}
-        <View className='my-2 w-full'>
-          <Text className='font-bold text-xl'>Friends Quest</Text>
-          <Text className='text-sm text-gray-500'>3 DAYS</Text>
-          <Text>Complete 5 lessons</Text>
-          <Text>2 / 5</Text>
-          <Text>You </Text>
-          <Text>1 lession</Text>
-          <Text>John Doe</Text>
-          <Text>0 lessons</Text>
-          <View className='flex-row justify-around'>
-            <TouchableOpacity className='bg-primary px-4 py-2 rounded-md'>
-              <Text className='text-white'>Nudge</Text>
-            </TouchableOpacity>
-            <TouchableOpacity className='bg-primary px-4 py-2 rounded-md'>
-              <Text className='text-white'>Gift</Text>
-            </TouchableOpacity>
+        <View className='my-4 w-full'>
+          {/* Header */}
+          <View className='flex-row justify-between items-center'>
+            <Text className='font-bold text-xl'>Friends Quest</Text>
+            <View className='flex-row'>
+              <MaterialIcons className='mr-2' name="timelapse" size={20} color={colors.primary} />
+              <Text className='text-sm text-gray-500'>3 DAYS</Text>
+            </View>
           </View>
+
+          {/* Friend Quest progress */}
+          <View className='w-[90%] mx-auto my-2'>
+            <Text className='text-xl mb-2'>Complete 5 lessons</Text>
+            <View className='flex-1 justify-center items-center'>
+            {/* progress bar */}
+              <View className='flex-1 h-6 bg-gray-300 rounded-full w-[80%] justify-center items-center'>
+                <Text className='text-textPrimary z-10'>3 / 5</Text>
+                <View className='absolute left-0 h-6 bg-primary rounded-l-full ' style={{ width: '60%' }} >
+                </View>
+              </View>
+            </View>
+          </View>
+
+          {/* Friend Quest status */}
+          <View className='flex-row justify-around items-center my-2'>
+            {/* Your status */}
+            <View className='flex justify-center items-center gap-1'>
+              <Image source={require("@/assets/avatars/penguin.png")} style={{width:100, height:100, borderRadius: 25 }}/>
+              <Text>You </Text>
+              <Text>1 lesson</Text>
+              <TouchableOpacity className='bg-primary px-4 py-2 rounded-md'>
+                <Text className='text-white text-center'>Nudge</Text>
+              </TouchableOpacity>
+            </View>
+
+            {/* Friend status */}
+            <View className='flex justify-center items-center gap-1'>
+              <Image source={require("@/assets/avatars/penguin.png")} style={{width:100, height:100, borderRadius: 25 }}/>
+              <Text>John Doe</Text>
+              <Text>0 lessons</Text>
+              <TouchableOpacity className='bg-primary px-4 py-2 rounded-md'>
+                <Text className='text-white text-center'>Gift</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+          
         </View>
 
         {/* Daily Quests */}
