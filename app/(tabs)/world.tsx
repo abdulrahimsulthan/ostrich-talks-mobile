806bd67244daf9d4a2a0e37d7422ebbe6b688826
abdulrahimsulthan <rahimcsestudies@gmail.com>
2025-05-13 17:58:06 +0530

    ✨ feat(world): world section
    Update conversation section to worlds with new options

--- app/(tabs)/world.tsx
+++ app/(tabs)/world.tsx
@@ -1,21 +1,29 @@
 import { View, Text, ScrollView, TouchableOpacity } from 'react-native'
 import React from 'react'
-import { MaterialIcons } from '@expo/vector-icons'
+import { FontAwesome5, MaterialIcons } from '@expo/vector-icons'
 
 const world = () => {
   return (
     <ScrollView className='bg-background'>
       <View className='w-[90%] mx-auto'>
-        {/* Conversation */}
+        {/* Worlds */}
         <View className='my-4 gap-4'>
-          <Text className='text-xl '>Conversation</Text>
-          <TouchableOpacity className='flex-row items-center justify-between bg-primary p-4 rounded-lg shadow-md'>
-            <Text className='text-white'>Speak</Text>
-            <MaterialIcons name='mic' color="white" size={24}/>
+          <Text className='text-xl '>Worlds</Text>
+          <TouchableOpacity className='flex-row items-center gap-4 bg-primary p-4 rounded-lg shadow-md'>
+            <MaterialIcons name='home-filled' color="white" size={24}/>
+            <Text className='text-white'>Home</Text>
           </TouchableOpacity>
-          <TouchableOpacity className='flex-row items-center justify-between bg-primary p-4 rounded-lg shadow-md'>
-            <Text className='text-white'>Listen</Text>
-            <MaterialIcons name='headphones' color="white" size={24}/>
+          <TouchableOpacity className='flex-row items-center gap-4 bg-primary p-4 rounded-lg shadow-md'>
+            <MaterialIcons name='home-filled' color="white" size={24}/>
+            <Text className='text-white'>Office</Text>
+          </TouchableOpacity>
+          <TouchableOpacity className='flex-row items-center gap-4 bg-primary p-4 rounded-lg shadow-md'>
+            <FontAwesome5 name="journal-whills" size={24} color="white" />
+            <Text className='text-white'>Social</Text>
+          </TouchableOpacity>
+          <TouchableOpacity className='flex-row items-center gap-4 bg-primary p-4 rounded-lg shadow-md'>
+            <FontAwesome5 name="journal-whills" size={24} color="white" />
+            <Text className='text-white'>Leadership</Text>
           </TouchableOpacity>
         </View>
 
