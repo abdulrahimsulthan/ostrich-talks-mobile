62def9c0ea8c569ea753c1888612a2854cf08fc5
abdulrahimsulthan <rahimcsestudies@gmail.com>
2025-05-17 11:35:00 +0530

    ✨ feat(streak): constants
    Add streak levels and completed message constants

---
+++ constants/streak.ts
@@ -0,0 +1,29 @@
+const streakLevels = [
+  {
+    title: "Start your streak!",
+    message: "Do a lesson to start your day.",
+  },
+  {
+    title: "You got a Fiery Streak!",
+    message: "1 more lesson to get a hotter streak!",
+  },
+  {
+    title: "You got a Blazer streak!",
+    message: "2 lessons away from a hottest streak.",
+  },
+  {
+    title: "You got a Turbo streak!",
+    message: "3 more lessons to get a hotter streak!",
+  },
+  {
+    title: "You got a Inferno streak!",
+    message: "4 lessons away from the hottest streak.",
+  },
+];
+
+const completedMessage = {
+  title: "Got Supernova streak.",
+  message: "Great work! You make a very good day.",
+};
+
+export { streakLevels, completedMessage };
